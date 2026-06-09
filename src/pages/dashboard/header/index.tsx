86afdd5cbdebc8e0e475f465579965043357e43f
Morteza Dadashpour <mmorteza.778899@gmail.com>
2026-06-09 16:38:55 +0330

    fix: streamline font-face declarations and correct logo import case

--- src/pages/dashboard/header/index.tsx
+++ src/pages/dashboard/header/index.tsx
@@ -8,7 +8,7 @@ import {
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import LOGO from "../../../assets/Logo.svg";
+import LOGO from "../../../assets/logo.svg";
 import { SettingsMenu } from "./components/SettingsMenu";
 import { useAuth } from "../../../context/auth";
 import { LocationSearch } from "./components/LocationSearch";
